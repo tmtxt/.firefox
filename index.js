5312194006dc27cebf871702ff696168bac779e7
Truong <me@truongtx.me>
2016-06-21 15:27:37 +0700

    basic structure for index file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('dotfirefox home');
+const {Class} = require('sdk/core/heritage');
+const {Unknown, Factory} = require('sdk/platform/xpcom');
+
+const dotfirefox = {
+  // Component class required props
+  extends: Unknown,
+  get wrappedJSObject() {
+    return this;
+  },
+
+  hello: function() {
+    console.log('world');
+  }
+};
+
+// Create and register the factory
+const contractId = '@truongtx.me/dotfirefox';
+const DotFirefox = Class(dotfirefox);
+Factory({
+  contract: contractId,
+  Component: DotFirefox
+});
